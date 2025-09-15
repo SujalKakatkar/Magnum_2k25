ef0b6dd5524816c845ad14fdda9fdb5cb8e243ea
sujalkakatkar07 <sujalkakatkar54@gmail.com>
2025-09-15 14:44:24 +0530

    align ment

--- src/components/Developers.tsx
+++ src/components/Developers.tsx
@@ -7,13 +7,13 @@ import { motion } from 'motion/react'
 function Developers() {
  return (
   <div id='dev' className='min-h-[40vh] w-full flex justify-center items-center overflow-hidden'>
-   <div className=' w-2/3 xl:1/2 flex flex-col  justify-center items-center gap-6'>
+   <div className=' w-2/3 xl:1/2 flex flex-col text-center  justify-center items-center gap-6'>
     <motion.h1
      initial={{ opacity: 0, y: -20 }}
      whileInView={{ opacity: 1, y: 0 }}
      viewport={{ once: true }}
      transition={{ duration: 1.5, ease: "easeOut" }}
-     className='text-5xl font-bold w-3/4 xl:w-full text-white text-center mb-4'>Developers</motion.h1>
+     className='text-5xl font-bold  xl:w-full text-white text-center mb-4'>Developers</motion.h1>
     <ul className='w-3/4  flex flex-col lg:flex-row gap-5 lg:gap-0  justify-between items-center'>
      {devlopers?.map((dev, i) => (
       <li key={i} className='flex flex-col gap-1 justify-center items-center'>
